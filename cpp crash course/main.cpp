c88b91d93d85e52274076f68ea2d4d6563b7f95f
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-16 22:24:39 +0530

    Taking user input in cpp

--- cpp crash course/main.cpp
+++ cpp crash course/main.cpp
@@ -11,18 +11,27 @@ int main()
     // cout<<s23423a;
 
     // camelCase Notation
-    int marksInMaths=83;
-    cout<<"The marks of the student in maths is "<<marksInMaths;
+    // int marksInMaths=83;
+    // cout<<"The marks of the student in maths is "<<marksInMaths;
 
-    string harry = "harry";
-    short a;
-    int b= 89;
-    long c;
-    long long d;
-    float const score = 45.32;
-    double score2 = 45.322;
-    long double score3 = 45.332;
+    // string harry = "harry";
+    // short a;
+    // int b= 89;
+    // long c;
+    // long long d;
+    // float const score = 45.32;
+    // double score2 = 45.322;
+    // long double score3 = 45.332;
     // score = 34.2;
-    b = 34;
-    cout<<"The score is "<<score;
+    // b = 34;
+    // cout<<"The score is "<<score;
+    
+    int a, b;
+    cout<<"Enter first number: "<<endl;
+    cin>>a;
+
+    cout<<"Enter second number: "<<endl;
+    cin>>b;
+
+    cout<<"The Sum is: "<<a + b<<endl;
 }
