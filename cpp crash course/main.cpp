58b0bca77afd54fefb23072cefed112f992381fa
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-16 19:44:03 +0530

    variables

---
+++ cpp crash course/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+    cout<<"Next Line";
+    int a, b, c;
+    short s23423a=9;
+    cout<<s23423a;
+}
