87bb6e97f9e2a920a131529e86d6dd2e4b5f6e0b
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-16 22:18:53 +0530

    update main

--- cpp crash course/main.cpp
+++ cpp crash course/main.cpp
@@ -4,9 +4,25 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World"<<endl;
-    cout<<"Next Line";
-    int a, b, c;
-    short s23423a=9;
-    cout<<s23423a;
+    // cout<<"Hello World"<<endl;
+    // cout<<"Next Line";
+    // int a, b, c;
+    // short s23423a=9;
+    // cout<<s23423a;
+
+    // camelCase Notation
+    int marksInMaths=83;
+    cout<<"The marks of the student in maths is "<<marksInMaths;
+
+    string harry = "harry";
+    short a;
+    int b= 89;
+    long c;
+    long long d;
+    float const score = 45.32;
+    double score2 = 45.322;
+    long double score3 = 45.332;
+    // score = 34.2;
+    b = 34;
+    cout<<"The score is "<<score;
 }
