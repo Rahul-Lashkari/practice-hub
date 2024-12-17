546579e1ad8a5950977ebabbb75ad19505762026
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-17 18:42:59 +0530

    array in cpp

--- cpp crash course/main.cpp
+++ cpp crash course/main.cpp
@@ -94,12 +94,53 @@ int main()
     //     cout<<"The value of i is "<<i<<endl;
     // }
 
-    int a, b;
-    cout<<"Enter first number: "<<endl;
-    cin>>a;
+    // int a, b;
+    // cout<<"Enter first number: "<<endl;
+    // cin>>a;
 
-    cout<<"Enter Second number: "<<endl;
-    cin>>b;
-    cout<<"The function returned: "<<add(a,b);
+    // cout<<"Enter Second number: "<<endl;
+    // cin>>b;
+    // cout<<"The function returned: "<<add(a,b);
 
+    // ---
+    //     int arr[] = {1, 3, 6};
+    // // Array Index   0  1  2
+    //     // cout<<arr[1];
+    //     int marks[6];
+
+    //     for (int i = 0; i < 6; i++)
+    //     {
+    //         cout<<"Enter the marks of "<<i<<"th student"<<endl;
+    //         cin>>marks[i];
+    //     }
+
+    //     for (int i = 0; i < 6; i++)
+    //     {
+    //         cout<<"Marks of "<<i<<"th student is "<<marks[i]<<endl;
+    //     }
+
+    // int arr2d[2][3] = {  {1,2,3},
+    //                      {4,5,6}};
+
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+    //         cout<<"The value at "<<i<<","<<j<<" is "<<arr2d[i][j]<<endl;
+    //     }
+    // }
+    
+    int arr[] = {1,2,3};
+    int marks[6];
+
+    for(int i=1; i<6; i++)
+    {
+        cout<<"Enter the marks of "<<i<<"th student"<<endl;
+        cin>>marks[i];
+    }
+
+    for(int i=1; i<6; i++)
+    {
+        cout<<"Marks of "<<i<<"th student is "<<marks[i]<<endl;
+    }
 }
