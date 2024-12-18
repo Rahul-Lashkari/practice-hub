3f38a5668f6aeae58ec116be1110b24e00b75072
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-18 18:40:43 +0530

    ex3 Prime Number Checker
    Created a C++ program to check if a given number is prime. Implements basic control flow and iteration to determine primality.

--- cpp crash course/main.cpp
+++ cpp crash course/main.cpp
@@ -64,6 +64,14 @@ int factorial(int n) {
     return n * factorial(n - 1);
 }
 
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= n / 2; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 
 int main()
 {
@@ -227,10 +235,20 @@ int main()
     // reverseString(str);
     // return 0;
 
+    // int num;
+    // cout << "Enter a number: ";
+    // cin >> num;
+    // cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    // return 0;
+
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    if (isPrime(num)) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }
     return 0;
 
 }
