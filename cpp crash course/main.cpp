be3e3f153c3250d655881401e939972052c3d31b
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-21 16:54:48 +0530

    ex9 Calculate Sum of N Natural Numbers
    Implemented a C++ program to compute the sum of the first N natural numbers using a simple loop. Demonstrates basic iteration and arithmetic operations.

--- cpp crash course/main.cpp
+++ cpp crash course/main.cpp
@@ -343,19 +343,30 @@ int main()
     // cout << "Area of the rectangle: " << area << endl;
     // return 0;
 
-    int num1, num2;
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter the second number: ";
-    cin >> num2;
+    // int num1, num2;
+    // cout << "Enter the first number: ";
+    // cin >> num1;
+    // cout << "Enter the second number: ";
+    // cin >> num2;
 
-    if (num1 > num2) {
-        cout << num1 << " is larger." << endl;
-    } else if (num2 > num1) {
-        cout << num2 << " is larger." << endl;
-    } else {
-        cout << "Both numbers are equal." << endl;
+    // if (num1 > num2) {
+    //     cout << num1 << " is larger." << endl;
+    // } else if (num2 > num1) {
+    //     cout << num2 << " is larger." << endl;
+    // } else {
+    //     cout << "Both numbers are equal." << endl;
+    // }
+    // return 0;
+
+    int n, sum = 0;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
     }
+
+    cout << "Sum of first " << n << " natural numbers is: " << sum << endl;
     return 0;
 
 }
