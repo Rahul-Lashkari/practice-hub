3bc8ef3fd196cf47d8403f3ca157f41179a715c4
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-22 16:49:43 +0530

    ex14

--- cpp crash course/main.cpp
+++ cpp crash course/main.cpp
@@ -407,17 +407,34 @@ int main()
     // cout << "The largest number is: " << largest << endl;
     // return 0;
 
-    int number, reverse = 0, remainder;
-    cout << "Enter an integer: ";
-    cin >> number;
+    // int number, reverse = 0, remainder;
+    // cout << "Enter an integer: ";
+    // cin >> number;
 
-    while (number != 0) {
-        remainder = number % 10;
-        reverse = reverse * 10 + remainder;
-        number /= 10;
+    // while (number != 0) {
+    //     remainder = number % 10;
+    //     reverse = reverse * 10 + remainder;
+    //     number /= 10;
+    // }
+
+    // cout << "Reversed number: " << reverse << endl;
+    // return 0;
+
+    int n;
+    unsigned long long factorial = 1;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        for (int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " is: " << factorial << endl;
     }
 
-    cout << "Reversed number: " << reverse << endl;
     return 0;
 
 }
