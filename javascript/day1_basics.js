f7256a5c7134bda738b8bf82f1a129d83569aa83
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-26 21:38:50 +0530

    JS Basics Day 1 - Variables and Constants in js
    Practiced declaring variables using let and const. Demonstrated reassignment and immutability with examples.

---
+++ javascript/day1_basics.js
@@ -0,0 +1,17 @@
+// Summary: JavaScript Basics Day 1
+// Description: Introduced JavaScript basics: variables, data types, functions, conditional statements, and loops. Practiced hands-on examples for understanding key concepts.
+// Today's Agenda — We'll cover basic concepts and syntax to get familiar with JavaScript.
+
+
+// Step 1: Variables and Constants :-
+
+// Declare variables using let and const
+let age = 25; // Variable that can be reassigned
+const name = "John"; // Constant variable
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Reassign age
+age = 26;
+console.log("Updated Age:", age);
