c2720dbe827a8ccabd5f0bd77959ca2eaaeed269
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-27 18:13:37 +0530

    Practiced JavaScript Operators
    Explored arithmetic, comparison, and logical operators with practical examples

--- javascript/day2.js
+++ javascript/day2.js
@@ -1,6 +1,8 @@
 // JS Day 2
 // Step 1: Variables and Data Types Recap
 
+/*
+--------------------------------------------------------------------------------------------------------------
 // Variables and Data Types
 let name = "Alice"; // string
 const age = 25; // number
@@ -9,3 +11,15 @@ let address; // undefined
 let notApplicable = null; // null
 
 console.log(name, age, isStudent, address, notApplicable);
+--------------------------------------------------------------------------------------------------------------
+*/
+// Operators in JavaScript
+// Operators
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Modulus:", num1 % num2);
