446047b489bc401eeb3e353ff4022ef664c770ea
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-27 22:30:28 +0530

    Day 1: Designed students table
    Created the 'students' table with fields for ID, name, age, and grade.

--- mysql/day1.sql
+++ mysql/day1.sql
@@ -3,3 +3,14 @@
 -- Step 1: Create Database
 CREATE DATABASE mydb;
 USE mydb;
+
+-- --------------------------------------------------------------------------------------------------------------
+
+-- Step 2: Create Table
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    grade VARCHAR(10)
+);
+
