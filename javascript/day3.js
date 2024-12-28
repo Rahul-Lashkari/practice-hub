edeb35a1377b824be71b07bed404340eab4bd44d
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-28 09:29:55 +0530

    Rest and Spread Operators
    Explored rest and spread operators to handle dynamic arguments and array manipulation.

--- javascript/day3.js
+++ javascript/day3.js
@@ -18,10 +18,22 @@ const add = (a, b) => a + b;
 
 console.log(add(5, 3)); // Outputs: 8
 --------------------------------------------------------------------------------------------------------------
-*/
 // Template Literals Example
 const name = "Bob";
 const age = 25;
 
 console.log(`My name is ${name} and I am ${age} years old.`);
+--------------------------------------------------------------------------------------------------------------
+*/
+// Rest Operator
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
 
+console.log(sum(1, 2, 3, 4)); // Outputs: 10
+
+// Spread Operator
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5];
+
+console.log(arr2); // Outputs: [1, 2, 3, 4, 5]
