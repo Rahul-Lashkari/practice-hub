acd3ef8ff1a8aaa395f1290c9265df98c8558fa7
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-31 16:10:12 +0530

    JS Day6 - Practiced basic array operations.
    Created a favorite movies array and performed push, pop, and display operations.

---
+++ javascript/day6.js
@@ -0,0 +1,21 @@
+// JavaScript - Day 6 🚀
+
+/*
+Today's focus:
+arrays and array methods in JavaScript.
+Understand different types of functions (named, anonymous, arrow).
+Practice creating and calling functions with real-world examples.
+*/
+
+// Creating and Manipulating Arrays :-
+// Creating an array
+let favoriteMovies = ["Inception", "The Matrix", "Interstellar"];
+
+// Adding a movie
+favoriteMovies.push("Parasite");
+
+// Removing the last movie
+favoriteMovies.pop();
+
+// Display the array
+console.log("My Favorite Movies:", favoriteMovies);
