9109ae47a740fa2a3f5538dee719eed50c8e6963
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2024-12-31 16:14:00 +0530

    Explored array iteration methods..
    Used forEach and for...of loops to iterate over an array of movies.

--- javascript/day6.js
+++ javascript/day6.js
@@ -7,6 +7,8 @@ Understand different types of functions (named, anonymous, arrow).
 Practice creating and calling functions with real-world examples.
 */
 
+/*
+--------------------------------------------------------------------------------------------------------------
 // Creating and Manipulating Arrays :-
 // Creating an array
 let favoriteMovies = ["Inception", "The Matrix", "Interstellar"];
@@ -19,3 +21,16 @@ favoriteMovies.pop();
 
 // Display the array
 console.log("My Favorite Movies:", favoriteMovies);
+--------------------------------------------------------------------------------------------------------------
+*/
+// Iterating Over Arrays :-
+let favoriteMovies = ["Inception", "The Matrix", "Interstellar"];
+// Using forEach
+favoriteMovies.forEach((movie, index) => {
+    console.log(`${index + 1}: ${movie}`);
+});
+
+// Using for...of loop
+for (let movie of favoriteMovies) {
+    console.log(movie);
+}
